9fc89c40dc6f56601fb002ac831817a74e8db9b2
mheavey2 <mheavey2@gmail.com>
2025-10-09 14:26:46 +0100

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+
 import App from "./components/App/App";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
+import WorkWithMe from "./components/WorkWithMe/WorkWithMe";
+import Hero from "./components/Hero/Hero";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Hero />} />
+      <Route path="about" element={<About />} />
+      <Route path="skills" element={<Skills />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="contact" element={<WorkWithMe />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>
 );
