ba5c61ddf6fe0e377ff2717e87f72e0ea06f8728
mheavey2 <mheavey2@gmail.com>
2025-11-12 16:31:20 +0000

    add reset scroll functionality to ensure page is scrolled to top on load/ reload

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,42 +2,29 @@ import styles from "./App.module.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import * as Scroll from "react-scroll";
+
+import { useLayoutEffect } from "react";
 
 function App() {
-  //use react-scroller to scroll to different page sections
-  const scroll = Scroll.animateScroll;
   const navigate = useNavigate();
-  const path = useLocation().pathname;
+  const location = useLocation();
+  const path = location.pathname;
 
-  // scroll to home
-  const scrollToHome = () => {
-    scroll.scrollToTop({
-      duration: 200,
-      delay: 0,
-      smooth: true,
-      offset: 0,
-    });
-  };
+  // scroll to top of page after a page transition
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(
+      { top: 0, left: 0, behaviour: "instant" },
+      [location.pathname]
+    );
+  });
 
   const goToHome = () => {
     navigate("/");
-    scrollToHome();
   };
   return (
     <div className={styles.appContainer}>
-      <Header
-        path={path}
-        scrollToHome={scrollToHome}
-        goToHome={goToHome}
-        navigate={navigate}
-      />
-      <Outlet
-        path={path}
-        scrollToHome={scrollToHome}
-        goToHome={goToHome}
-        navigate={navigate}
-      />
+      <Header path={path} goToHome={goToHome} navigate={navigate} />
+      <Outlet path={path} goToHome={goToHome} navigate={navigate} />
       <Footer />
     </div>
   );
